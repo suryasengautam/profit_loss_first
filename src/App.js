ed8ce92dbfc5f5a24a7a392eecdf822336893de7
suryasengautam <94212580+suryasengautam@users.noreply.github.com>
2021-12-05 17:53:00 +0530

    second time change in code

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import "./styles.css";
 
 export default function App() {
+  function changeClickHandler1(event) {
+    var buy = event.target.value;
+    console.log(buy);
+  }
+  function changeClickHandler1(event) {
+    var buy = event.target.value;
+    console.log(buy);
+  }
+  function changeClickHandler2(event) {
+    var buy = event.target.value;
+    console.log(buy);
+  }
+
+  function changeClickHandler3(event) {
+    var sp = event.target.value;
+    console.log(buy);
+  }
   return (
     <div className="App">
       <h1> Stock Profile & Loss Calculator </h1>
       <h2> initial price </h2>
       <p>
-        <input placeHolder = "enter initial price"/>
+        <input
+          onChange={changeClickHandler1}
+          placeHolder="enter initial price"
+        />
       </p>
       <h2> Quantity of Stocks </h2>
-      <input placeHolder = "enter quantity of stock "/>
+      <input
+        onChange={changeClickHandler2}
+        placeHolder="enter quantity of stock "
+      />
       <h1>Current Price</h1>
-      <input placeHolder = "enter current price"/>
+      <input placeHolder="enter current price" />
       <br />
-      <bitton> tell me </bitton>
+      <button onChange={changeClickHandler3} />
     </div>
   );
 }
